605e265158fb964ec996dac14368028bd8c5a285
DimitrisDimitriadis <dimitris.dimtriadis@gmail.com>
2017-01-13 06:46:18 +0200

    questsV2.0 | removed unnecessary code | updated quest functionality | quest marks dissapear after they've been answered correctly

---
+++ FrontEnd/app/src/main/java/com/example/johnywalker/adventure_go/miscellaneous/CompareStrings.java
@@ -0,0 +1,44 @@
+package com.example.johnywalker.adventure_go.miscellaneous;
+
+/**
+ * Created by Dimitriadis983 on 13-Jan-17.
+ */
+
+public class CompareStrings
+{
+    public boolean compareStrings(String string1, String string2)
+    {
+        if(string1.equals(string2))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public boolean strictCompareStrings(String string1, String string2)
+    {
+        string1 = modifyString(string1);
+        string2 = modifyString(string2);
+
+        if(string1.equals(string2))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private String modifyString(String s)
+    {
+        String result = s;
+        result = result.toUpperCase();
+        result = result.trim();
+
+        return result;
+    }
+}
